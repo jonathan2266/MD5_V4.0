649c824598ae928c86c9bab19ee34124c8159170
jonathan <jonathan2266@hotmail.be>
2016-03-08 11:56:16 +0100

    Bugfix in Wordgenerator class -> specific the jump from reference class. The jumpig foreward was bugged. Also removed useless text.

--- MD5_V4.0_C/wordGenerator.cs
+++ MD5_V4.0_C/wordGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace MD5_V2
 {
@@ -81,202 +82,6 @@ namespace MD5_V2
             for (int i = 0; i < reversed.Length; i++)
             {
                 builder.Append(l.NumberToString(reversed[i]));
-                #region //switch turned off
-                //switch (reversed[i])
-                //{
-                //    case 1:
-                //        builder.Append("a");
-                //        break;
-                //    case 2:
-                //        builder.Append("b");
-                //        break;
-                //    case 3:
-                //        builder.Append("c");
-                //        break;
-                //    case 4:
-                //        builder.Append("d");
-                //        break;
-                //    case 5:
-                //        builder.Append("e");
-                //        break;
-                //    case 6:
-                //        builder.Append("f");
-                //        break;
-                //    case 7:
-                //        builder.Append("g");
-                //        break;
-                //    case 8:
-                //        builder.Append("h");
-                //        break;
-                //    case 9:
-                //        builder.Append("i");
-                //        break;
-                //    case 10:
-                //        builder.Append("j");
-                //        break;
-                //    case 11:
-                //        builder.Append("k");
-                //        break;
-                //    case 12:
-                //        builder.Append("l");
-                //        break;
-                //    case 13:
-                //        builder.Append("m");
-                //        break;
-                //    case 14:
-                //        builder.Append("n");
-                //        break;
-                //    case 15:
-                //        builder.Append("o");
-                //        break;
-                //    case 16:
-                //        builder.Append("p");
-                //        break;
-                //    case 17:
-                //        builder.Append("q");
-                //        break;
-                //    case 18:
-                //        builder.Append("r");
-                //        break;
-                //    case 19:
-                //        builder.Append("s");
-                //        break;
-                //    case 20:
-                //        builder.Append("t");
-                //        break;
-                //    case 21:
-                //        builder.Append("u");
-                //        break;
-                //    case 22:
-                //        builder.Append("v");
-                //        break;
-                //    case 23:
-                //        builder.Append("w");
-                //        break;
-                //    case 24:
-                //        builder.Append("x");
-                //        break;
-                //    case 25:
-                //        builder.Append("y");
-                //        break;
-                //    case 26:
-                //        builder.Append("z");
-                //        break;
-                //    case 27:
-                //        builder.Append("A");
-                //        break;
-                //    case 28:
-                //        builder.Append("B");
-                //        break;
-                //    case 29:
-                //        builder.Append("C");
-                //        break;
-                //    case 30:
-                //        builder.Append("D");
-                //        break;
-                //    case 31:
-                //        builder.Append("E");
-                //        break;
-                //    case 32:
-                //        builder.Append("F");
-                //        break;
-                //    case 33:
-                //        builder.Append("G");
-                //        break;
-                //    case 34:
-                //        builder.Append("H");
-                //        break;
-                //    case 35:
-                //        builder.Append("I");
-                //        break;
-                //    case 36:
-                //        builder.Append("J");
-                //        break;
-                //    case 37:
-                //        builder.Append("K");
-                //        break;
-                //    case 38:
-                //        builder.Append("L");
-                //        break;
-                //    case 39:
-                //        builder.Append("M");
-                //        break;
-                //    case 40:
-                //        builder.Append("N");
-                //        break;
-                //    case 41:
-                //        builder.Append("O");
-                //        break;
-                //    case 42:
-                //        builder.Append("P");
-                //        break;
-                //    case 43:
-                //        builder.Append("Q");
-                //        break;
-                //    case 44:
-                //        builder.Append("R");
-                //        break;
-                //    case 45:
-                //        builder.Append("S");
-                //        break;
-                //    case 46:
-                //        builder.Append("T");
-                //        break;
-                //    case 47:
-                //        builder.Append("U");
-                //        break;
-                //    case 48:
-                //        builder.Append("V");
-                //        break;
-                //    case 49:
-                //        builder.Append("W");
-                //        break;
-                //    case 50:
-                //        builder.Append("X");
-                //        break;
-                //    case 51:
-                //        builder.Append("Y");
-                //        break;
-                //    case 52:
-                //        builder.Append("Z");
-                //        break;
-                //    case 53:
-                //        builder.Append(" ");
-                //        break;
-                //    case 54:
-                //        builder.Append("0");
-                //        break;
-                //    case 55:
-                //        builder.Append("1");
-                //        break;
-                //    case 56:
-                //        builder.Append("2");
-                //        break;
-                //    case 57:
-                //        builder.Append("3");
-                //        break;
-                //    case 58:
-                //        builder.Append("4");
-                //        break;
-                //    case 59:
-                //        builder.Append("5");
-                //        break;
-                //    case 60:
-                //        builder.Append("6");
-                //        break;
-                //    case 61:
-                //        builder.Append("7");
-                //        break;
-                //    case 62:
-                //        builder.Append("8");
-                //        break;
-                //    case 63:
-                //        builder.Append("9");
-                //        break;
-                //    default:
-                //        break;
-                //}
-                #endregion turned off
             }
             text = Convert.ToString(builder);
         }
@@ -288,214 +93,10 @@ namespace MD5_V2
             for (int i = 0; i < entry.Length; i++)
             {
                 listOfNumbers.Add(l.StringToNumber(entry[i]));
-                //listOfNumbers.Add(index(entry[i]));
                 
             }
             listOfNumbers.Reverse();
         }
-
-        //private int index(char v)
-        //{
-        //    int temp = 0;
-        //    #region //switch
-        //    switch (v)
-        //    {
-        //        case 'a':
-        //            temp = 1;
-        //            break;
-        //        case 'b':
-        //            temp = 2;
-        //            break;
-        //        case 'c':
-        //            temp =3;
-        //            break;
-        //        case 'd':
-        //            temp =4;
-        //            break;
-        //        case 'e':
-        //            temp =5;
-        //            break;
-        //        case 'f':
-        //            temp =6;
-        //            break;
-        //        case 'g':
-        //            temp =7;
-        //            break;
-        //        case 'h':
-        //            temp =8;
-        //            break;
-        //        case 'i':
-        //            temp = 9;
-        //            break;
-        //        case 'j':
-        //            temp =10;
-        //            break;
-        //        case 'k':
-        //            temp =11;
-        //            break;
-        //        case 'l':
-        //            temp = 12;
-        //            break;
-        //        case 'm':
-        //            temp = 13;
-        //            break;
-        //        case 'n':
-        //            temp = 14;
-        //            break;
-        //        case 'o':
-        //            temp = 15;
-        //            break;
-        //        case 'p':
-        //            temp = 16;
-        //            break;
-        //        case 'q':
-        //            temp = 17;
-        //            break;
-        //        case 'r':
-        //            temp =18;
-        //            break;
-        //        case 's':
-        //            temp = 19;
-        //            break;
-        //        case 't':
-        //            temp =20;
-        //            break;
-        //        case 'u':
-        //            temp =21;
-        //            break;
-        //        case 'v':
-        //            temp =22;
-        //            break;
-        //        case 'w':
-        //            temp =23;
-        //            break;
-        //        case 'x':
-        //            temp =24;
-        //            break;
-        //        case 'y':
-        //            temp =25;
-        //            break;
-        //        case 'z':
-        //            temp =26;
-        //            break;
-        //        case 'A':
-        //            temp =27;
-        //            break;
-        //        case 'B':
-        //            temp =28;
-        //            break;
-        //        case 'C':
-        //            temp =29;
-        //            break;
-        //        case 'D':
-        //            temp =30;
-        //            break;
-        //        case 'E':
-        //            temp =31;
-        //            break;
-        //        case 'F':
-        //            temp =32;
-        //            break;
-        //        case 'G':
-        //            temp =33;
-        //            break;
-        //        case 'H':
-        //            temp =34;
-        //            break;
-        //        case 'I':
-        //            temp =35;
-        //            break;
-        //        case 'J':
-        //            temp =36;
-        //            break;
-        //        case 'K':
-        //            temp =37;
-        //            break;
-        //        case 'L':
-        //            temp =38;
-        //            break;
-        //        case 'M':
-        //            temp =39;
-        //            break;
-        //        case 'N':
-        //            temp =40;
-        //            break;
-        //        case 'O':
-        //            temp =41;
-        //            break;
-        //        case 'P':
-        //            temp =42;
-        //            break;
-        //        case 'Q':
-        //            temp =43;
-        //            break;
-        //        case 'R':
-        //            temp =44;
-        //            break;
-        //        case 'S':
-        //            temp =45;
-        //            break;
-        //        case 'T':
-        //            temp =46;
-        //            break;
-        //        case 'U':
-        //            temp =47;
-        //            break;
-        //        case 'V':
-        //            temp =48;
-        //            break;
-        //        case 'W':
-        //            temp =49;
-        //            break;
-        //        case 'X':
-        //            temp =50;
-        //            break;
-        //        case 'Y':
-        //            temp =51;
-        //            break;
-        //        case 'Z':
-        //            temp =52;
-        //            break;
-        //        case ' ':
-        //            temp =53;
-        //            break;
-        //        case '0':
-        //            temp =54;
-        //            break;
-        //        case '1':
-        //            temp =55;
-        //            break;
-        //        case '2':
-        //            temp =56;
-        //            break;
-        //        case '3':
-        //            temp =57;
-        //            break;
-        //        case '4':
-        //            temp =58;
-        //            break;
-        //        case '5':
-        //            temp =59;
-        //            break;
-        //        case '6':
-        //            temp =60;
-        //            break;
-        //        case '7':
-        //            temp =61;
-        //            break;
-        //        case '8':
-        //            temp =62;
-        //            break;
-        //        case '9':
-        //            temp =63;
-        //            break;
-        //        default:
-        //            temp = 1;
-        //            break;
-        //    }
-        //    #endregion
-        //    return temp;
-        //}
     }
 
     public class wordXFromReference
@@ -516,7 +117,7 @@ namespace MD5_V2
             string word = "";
             char[] charReferenceWord = referenceWord.ToCharArray();
             int[] intForm = new int[charReferenceWord.Length];
-            int solidIntForm = 0;
+            long solidIntForm = 0;
 
             for (int i = 0; i < charReferenceWord.Length; i++)
             {
@@ -539,16 +140,16 @@ namespace MD5_V2
             return word;
         }
 
-        private int[] SolidToIntArray(int solid)
+        private int[] SolidToIntArray(long solid)
         {
             int[] value;
             int power = 0;
-            int check;
+            long check;
             bool run = true;
 
             while (run)
             {
-                check = solid - (int)(Math.Pow(l.IndexLenght, power));
+                check = solid - (long)(Math.Pow(l.IndexLenght, power));
                 if (check > 0)
                 {
                     power++;
@@ -557,14 +158,20 @@ namespace MD5_V2
                 {
                     break;
                 }
-                if (check < 0)
+                if (check <= 0)
                 {
                     power--;
                     break;
                 }
             }
 
-            List<int> tempList = new List<int>();
+            //List<int> tempList = new List<int>();
+            int[] tempList = new int[power + 1];
+
+            for (int i = 0; i < tempList.Length; i++)
+            {
+                tempList[i] = 1;
+            }
             int loopCount = 0;
 
             while (true)
@@ -577,19 +184,19 @@ namespace MD5_V2
                 }
                 if (check < 0)
                 {
+                    tempList[power] = loopCount;
                     power--;
-                    tempList.Add(loopCount);
                     loopCount = 0;
                 }
                 if (check == 0)
                 {
                     loopCount++;
-                    tempList.Add(loopCount);
+                    tempList[power] = loopCount;
                     break;
                 }
             }
-
-            value = tempList.ToArray();
+            value = tempList;
+            Array.Reverse(value);
             return value;
         }
         private int intArrayToSolidInt(int[] array)
