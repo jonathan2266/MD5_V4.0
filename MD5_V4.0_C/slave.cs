4dda7b28ae36d47755144b004158cb7469745b52
jonathan <jonathan2266@hotmail.be>
2016-03-21 15:06:03 +0100

    rewrote slave->recieveJob. That old code was just a mess. hopefully this wil fix the weird bug that somethime showed up

--- MD5_V4.0_C/slave.cs
+++ MD5_V4.0_C/slave.cs
@@ -18,13 +18,12 @@ namespace MD5_V4._0_C
         private StringBuilder[] listOfHash;
         private int[] startNr;
         private int[] status;  // 0 = not finished 1 = finished (creating the hash list) 2 = written to master
+        private int subCount;
 
-        private int GenericCounter;
         public slave(int port)
         {
             this.port = port;
             listenToMaster();
-            GenericCounter = 0;
         }
         private void listenToMaster()
         {
@@ -51,15 +50,13 @@ namespace MD5_V4._0_C
             TList = new BackgroundWorker[threads];
             startNr = new int[threads];
             status = new int[threads];
+            subCount = 0;
 
             word = s.Recieve();
             if (word == "*")
             {
                 word = ""; //cant recieve an empty byte maybe it will when i add :: when sending
             }
-
-            GenericCounter = -1; //gg if this ever overflows :p
-            int count = -1; //offset compared to word //or max of 40 parts maybe??
 
             for (int i = 0; i < threads; i++)
             {
@@ -75,139 +72,81 @@ namespace MD5_V4._0_C
 
             while (working)
             {
-                A:
-
-                //we look for the lowest startNr
-                int lowest = startNr[0];
-                int nrLowest = 0;
-                for (int i = 0; i < startNr.Length; i++)
-                {
-                    if (lowest > startNr[i])
-                    {
-                        lowest = startNr[i];
-                        nrLowest = i;
-                    }
-                }
-
-                //Now we look if we can do anything with the corresponding data if not we wait!!
-                if (lowest != int.MaxValue)
-                {
-                    if (status[nrLowest] == 1) //finished but not written
-                    {
-                        s.SendStuff(listOfHash[nrLowest].ToString());
-                        listOfHash[nrLowest].Clear();
-                        status[nrLowest] = 2; //written to master
-                        startNr[nrLowest] = int.MaxValue;
-                    }
-                }
-
 
                 for (int i = 0; i < TList.Length; i++)
                 {
                     if (!TList[i].IsBusy)
                     {
-                        int Genericnumber = -1;
-                        for (int j = 0; j < listOfHash.Length; j++)
+                        //can we give it a new job? or do we have to wait? (statuscheck)
+                        if (status[i] == 2 && subCount < 400) //meaning the thread is free so give it a job
                         {
-                            if (status[j] == 2)
-                            {
-                                Genericnumber = j;
-                                break;
-                            }
+                            startSub(i);
                         }
-
-                        if (Genericnumber == -1)
+                        if (status[i] == 1) //meaning it if finished but can it be writen? everything has to be send in order
                         {
-                            Thread.Sleep(2);
-                            goto A;// maybe add a delay
+                            writeSub(i);
                         }
-
-                        count++;
-                        if (count >= 400)
+                    }
+                }
+                if (subCount <= 400)
+                {
+                    int emptyCount = 0;
+                    for (int i = 0; i < status.Length; i++)
+                    {
+                        if (status[i] == 2)
                         {
-                            WaitUntilDone();
-                            working = false;
-                            break;
+                            emptyCount++;
                         }
-
-                        GenericCounter++;
-                        startNr[i] = GenericCounter;
-                        Console.WriteLine("GenericCounter: " + GenericCounter);
-                        for (int z = 0; z < startNr.Length; z++)
-                        {
-                            Console.WriteLine(startNr[z]);
-                        }
-
-                        object[] threadInfo = new object[3];
-                        threadInfo[0] = i;
-                        threadInfo[1] = word;
-                        threadInfo[2] = Genericnumber;
-
-                        //now change word to the next one 1250000/400
-                        status[Genericnumber] = 0;
-                        TList[i].RunWorkerAsync(threadInfo);
-                        wordXFromReference x = new wordXFromReference(word, 31250);
-                        word = x.DoJump();
-
+                    }
+                    if (emptyCount == threads)
+                    {
+                        working = false;
                     }
                 }
                 Thread.Sleep(10);
             }
         }
 
-        private void WaitUntilDone()
+        private void writeSub(int nr)
         {
-            Console.WriteLine("WaituntilDone");
-
-            B:
-            int DoneCounter = 0;
-            for (int i = 0; i < TList.Length; i++)
-            {
-                if (!TList[i].IsBusy)
-                {
-                    DoneCounter++;
-                    Console.WriteLine(DoneCounter);
-                }
-            }
-            if (DoneCounter != TList.Length)
-            {
-                goto B;
-            }
-
-            //now we have to write everything. this is double code too :D
-            C:
-
-            int lowest = startNr[0];
-            int nrLowest = 0;
+            //need to see if the lowest startNr is the same is the int recieved from method
+            int lowestIndex = 0;
             for (int i = 0; i < startNr.Length; i++)
             {
-                if (lowest > startNr[i])
+                if (startNr[i] < startNr[lowestIndex])
                 {
-                    lowest = startNr[i];
-                    nrLowest = i;
+                    lowestIndex = i;
                 }
             }
 
-            if (lowest != int.MaxValue)
+            if (lowestIndex == nr) //can write
             {
-                s.SendStuff(listOfHash[nrLowest].ToString());
-                listOfHash[nrLowest].Clear();
-                startNr[nrLowest] = int.MaxValue;
-                goto C;
+                s.SendStuff(listOfHash[nr].ToString());
+                listOfHash[nr].Clear();
+                status[nr] = 2;
             }
+        }
 
-            //now wait in a response from server
-            Console.WriteLine("RecieveNewJob");
-            //recieveJob();
+        private void startSub(int nr)
+        {
+            status[nr] = 0;
+            startNr[nr] = subCount;
 
+            object[] threadInfo = new object[2];
+            threadInfo[0] = nr;
+            threadInfo[1] = word;
+            TList[nr].RunWorkerAsync(threadInfo);
 
-
+            wordXFromReference x = new wordXFromReference(word, 31250);
+            word = x.DoJump();
+            subCount++; //until 400
+            
         }
 
         private void Bw_DoWork(object sender, DoWorkEventArgs e)
         {
             object[] info = e.Argument as object[];
-            int nr = (int)info[2];
+            int nr = (int)info[0];
             string word = (string)info[1];
             if (word == null)
             {
